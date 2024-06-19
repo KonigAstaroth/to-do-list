d38a327f46d80dc9a9c778081a063a410c4f92af
Guillermo Gonzalez <guillermogonzalezg2005@gmail.com>
2024-06-19 08:31:24 -0600

    Agregado quitar y poner elementos

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
+let IdCounter = 0;
+const input = document.querySelector('input[type="text"]');
 userInput.addEventListener('submit', (event)=>{
     event.preventDefault();
-    addtask();
+    addTask();
 });
     
-let addtask = ()=>{
+let addTask = ()=>{
+    IdCounter ++;
+ 
+    let newValue =input.value;
     list.innerHTML += `
-    <div id ="list" class="list-container">
+    <div class="task-container" id="$(IdCounter)">
+        <label>
+            <input type="checkbox">
+            ${newValue}
+        </label>
+        <img src="/bin.png" class="clBtn">
+    </div>
+     `  
+     input.value= ''; 
+     updatestats();
+}; 
 
-            <div class="task-container" id="1">
-                <label>
-                    <input type="checkbox">
-                    Tarea 1
-                </label>
-            </div>
+list.addEventListener('click', (event)=>{
+    if(event.srcElement.nodeName == 'INPUT'){
+        updatestats();
+    }else if(event.srcElement.nodeName == 'IMG'){
+        deleteTask(event.srcElement.parentNode.id);
+       }
+   
+});
 
-        </div>
-     `   
-};
+let updatestats = ()=>{
+    let element = list.querySelectorAll('div');
+    let checkbox = list.querySelectorAll('input[type="checkbox"]:checked');
+    stats.innerHTML = `<p>Tareas agregadas: ${element.length} / Completadas: ${checkbox.length}</p>`;
+}
+
+let deleteTask = (id)=>{
+    let taskToDelete = document.getElementById(id);
+    list.removeChild(taskToDelete);
+    updatestats();
+}
